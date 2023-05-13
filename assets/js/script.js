a66621a38296410f2de1edbe59b0e7d896147674
Ricardo Guerra <rj.guerra@outlook.com>
2023-05-13 19:07:58 -0400

    Apply Past, Present, or Future Class to Each Time Block Fixes #2

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,8 @@ $(function () {
 
   const WORK_HOURS_PER_DAY =  9;                                /* Work days are typically 9 hours (including lunch)    */
   const WORK_HOUR_OFFSET   =  9;                                /* Work days typically start at 9am                     */
+  const DAYJS = dayjs();                                        /* Day.js Object                                        */
+  const CURRENT_HOUR = DAYJS.hour();                            /* Current hour                                         */
 
                                                                 /* ============= GENERATE TIME BLOCK DIVS ============= */
   for (let hour = 0; hour < WORK_HOURS_PER_DAY; hour++) 
@@ -37,19 +39,22 @@ $(function () {
     let timeBlockID         =    "hour-" + (hour + WORK_HOUR_OFFSET);
     let timeBlockClass      =    "row time-block ";
 
-                                                                /* Create time block element hour label                 */
+    
+                                                                /* ----------- BUILD TIME BLOCK HOUR LABEL ------------ */
     let timeBlockHourLabelElement = $("<div>", {
       class: "col-2 col-md-1 hour text-center py-3",
-      text: dayjs().hour((hour + WORK_HOUR_OFFSET)).format("h A")
+      text: DAYJS.hour((hour + WORK_HOUR_OFFSET)).format("h A")
     });
 
-                                                                /* Create time block text area element                  */
+
+                                                                /* ------------ BUILD TIME BLOCK TEXT AREA ------------ */
     let timeBlockTextAreaElement = $("<textarea>", {
       class: "col-8 col-md-10 description",
       rows: "3"
     });
 
-                                                                /* Create time block save button element                */
+
+                                                                /* ----------- BUILD TIME BLOCK SAVE BUTTON ----------- */
     let timeBlockSaveButtonElement = $("<button>", {
       class: "btn saveBtn col-2 col-md-1",
       "aria-label": "save"
@@ -63,9 +68,23 @@ $(function () {
 
     timeBlockSaveButtonElement.append(saveButtonIcon);          /* Append save icon to save button element              */
 
-                                                                /* Determine class of time block                        */
-    timeBlockClass += "past";
 
+                                                                /* ------------ DETERMINE TIME BLOCK CLASS ------------ */
+    if (CURRENT_HOUR > (hour + WORK_HOUR_OFFSET))
+    {
+      timeBlockClass += "past";                                 /* Assign "past" (gray bckgnd) class to past times      */
+    }
+    else if (CURRENT_HOUR == (hour + WORK_HOUR_OFFSET))
+    {
+      timeBlockClass += "present";                              /* Assign "present" (red bckgnd) class to present times */
+    }
+    else
+    {
+      timeBlockClass += "future";                               /* Assign "future" (green bckgnd) class to future times */
+    }
+
+
+                                                                /* ------------- BUILD TIME BLOCK ELEMENT ------------- */
     timeBlockElement.attr("id", timeBlockID);                   /* Set time block div ID                                */
     timeBlockElement.attr("class", timeBlockClass);             /* Set time block div class                             */
     timeBlockElement.append(timeBlockHourLabelElement);         /* Append time block hour label to time block           */
